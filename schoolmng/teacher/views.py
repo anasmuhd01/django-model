e6a3205a2aa205716463f7fcece7927456578abe
unknown <anasmuhd010@gmail.com>
2026-05-04 11:58:31 +0530

    second

--- schoolmng/teacher/views.py
+++ schoolmng/teacher/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class homeView(View):
+    def get(self,req):
+        return render(req,'home.html')
