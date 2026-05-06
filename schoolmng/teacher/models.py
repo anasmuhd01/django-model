52cd78c69eebc54f238f1ab05f826a33ad815272
unknown <anasmuhd010@gmail.com>
2026-05-06 11:46:12 +0530

    models.py

--- schoolmng/teacher/models.py
+++ schoolmng/teacher/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class StudentList(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    batch = models.CharField(max_length=50)
+    place = models.CharField(max_length=100)
