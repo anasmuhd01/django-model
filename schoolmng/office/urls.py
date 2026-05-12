2ca1df27a9d5361202cc6c8619d48d248ebf4ecc
unknown <anasmuhd010@gmail.com>
2026-05-12 12:28:02 +0530

    all files

--- schoolmng/office/urls.py
+++ schoolmng/office/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from office.views import *
 urlpatterns=[
-    path('',OfficeHomeView.as_view(),name="officehome")
+    path('',OfficeHomeView.as_view(),name="officehome"),
+    path('test',AdddeptView.as_view(),name="adddept"),
+    path('deptlist',DeptListView.as_view(),name="dptlistv")
 ]
--- schoolmng/office/views.py
+++ schoolmng/office/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
+from office.forms import * 
 
 # Create your views here.
 
 class OfficeHomeView(View):
     def get(self,req):
         return render(req,'officehome.html')
+    
+class AdddeptView(View):
+    def get(self,req):
+        form = DeptForm()
+        return render(req,"adddept.html",{'form':form})
+    def post(self,req):
+        form_data = DeptForm(data=req.POST)
+        if form_data.is_valid():
+            form_data.save()
+            return redirect('officehome')
+        
+class DeptListView(View):
+    def get(self,req):
+        data = Department.objects.all()
+        return render(req,"deptlist.html",{'data':data})
